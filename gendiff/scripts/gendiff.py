c1e16e36e8cb7cc40cdebce613b8b0b0b7388675
NeishchenkoAlex <br.cheg@mail.ru>
2025-03-07 02:25:19 +0300

    add logic step4

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,16 @@
-from gendiff.scripts.generate_diff import gen_diff as gendiff
+from gendiff.logic_gendiff import gen_diff
 import argparse
 def main():
-    #Название и описание
+    #Инит утилиты 
     parser = argparse.ArgumentParser(prog='gendiff',description='Compares two configuration files and shows a difference.')
     #позиционные аргументы
     parser.add_argument('first_file',help='- Enter first argument',type=str)
     parser.add_argument('second_file', help='- Enter second argument',type=str)
     #опциональные аргументы
     parser.add_argument('-f', '--format', help='set format of output')
-    
     args = parser.parse_args()
-    #вывод утилиты
-    print(gendiff(args.first_file, args.second_file))
+    
+    print(gen_diff(args.first_file, args.second_file))
 
     
     
