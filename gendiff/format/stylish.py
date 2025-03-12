5c87c4faf9f116c9bdd1cc131fcd09ed29f870e8
NeishchenkoAlex <br.cheg@mail.ru>
2025-03-12 16:26:26 +0300

    fix tetst

--- gendiff/format/stylish.py
+++
@@ -1,2 +0,0 @@
-def stylish(result):
-    
--- gendiff/gendiff_tree.py
+++ gendiff/gendiff_tree.py
@@ -2,17 +2,19 @@ from gendiff.logic_gendiff import load_file
 
 data1 = load_file('file3.json')
 data2 = load_file('file4.json')
+
+
 def build_tree(data1, data2):
-    result=[]
+    result = []
     all_keys = sorted(set(data1) | set(data2))
     for key in all_keys:
-        item1=data1.get(key)
-        item2=data2.get(key)
+        item1 = data1.get(key)
+        item2 = data2.get(key)
         if isinstance(item1, dict) and isinstance(item2, dict):
             result.append({'key': key, 'type': 'nested', 'children': build_tree(item1, item2)})
         elif key in data1 and key in data2:
-            if item1!=item2:
-                result.append({'key': key, 'type': 'changed','old_value': item1, 'new_value': item2})
+            if item1 != item2:
+                result.append({'key': key, 'type': 'changed', 'old_value': item1, 'new_value': item2})
             else:
                 result.append({'key': key, 'type': 'unchanged', 'value': item1})
         elif key in data1 and key not in data2:
@@ -21,7 +23,9 @@ def build_tree(data1, data2):
             result.append({'key': key, 'type': 'added', 'value': item2})
 
     return result
-print(build_tree(data1,data2))
+
+
+print(build_tree(data1, data2))
     
     
 
